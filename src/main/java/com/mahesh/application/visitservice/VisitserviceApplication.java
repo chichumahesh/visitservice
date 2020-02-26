60df00a7a79e89b81e96788cac61c24b9396774b
chichumahesh <maheramachandran@gmail.com>
2020-02-26 05:17:26 -0500

    adding visits service

---
+++ src/main/java/com/mahesh/application/visitservice/VisitserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.application.visitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahesh/application/visitservice/VisitserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.application.visitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
